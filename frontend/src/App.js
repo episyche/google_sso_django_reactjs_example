626364347eb8233f237856f9517596c6e8b510e0
Karventhan-K <karventhanalpm@gmail.com>
2022-08-30 18:16:48 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import GoogleAuth from './GoogleAuth';
+
+function App() {
+  return (
+    <div>
+      <GoogleAuth>
+      </GoogleAuth>
+    </div>
+  );
+}
+
+export default App;
